ad1f525b71333608e7f44138e4ddbeea4f5ab2ba
Ezequiel Blanco Mercado <ezeblamer@outlook.es>
2024-09-23 14:52:18 -0500

    PR feature/FoodSpin test1 js with carousel...is accessible

---
+++ index.js
@@ -0,0 +1,32 @@
+const carouselItems = document.querySelectorAll('.js-carousel__foordSpin-dishes');
+const prevButton = document.querySelector('.js-prev');
+const nextButton = document.querySelector('.js-next');
+
+let activeIndex = 0;
+
+prevButton.addEventListener('click', () => {
+    activeIndex--;
+    if (activeIndex < 0) {
+        activeIndex = carouselItems.length - 1;
+    }
+    updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+    activeIndex++;
+    if (activeIndex >= carouselItems.length) {
+        activeIndex = 0;
+    }
+    updateCarousel();
+});
+
+function updateCarousel() {
+    carouselItems.forEach((item, index) => {
+        item.classList.remove('active');
+        if (index === activeIndex) {
+            item.classList.add('active');
+        }
+    });
+}
+
+updateCarousel();
